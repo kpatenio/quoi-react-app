80a42a6e327301e3473f0d5114e71bf2b3d9906d
kpatenio <katherinepatenio@gmail.com>
2019-08-16 19:55:25 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+// TODO - convert into a state component!
 
 const App: React.FC = () => {
+  // TODO - change to proper state
+  const [testState, setTestState] = useState(null);
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/')
+    .then((response) => {
+      console.log(response);
+      setTestState(response.data);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p> {testState} </p>
+        {/* //TODO - what are target and rel? */}
         <a
           className="App-link"
           href="https://reactjs.org"
