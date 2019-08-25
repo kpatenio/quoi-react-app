2e0e8cdae62cd7fcd71749e748f48cb9110e8b9e
kpatenio <25045015+kpatenio@users.noreply.github.com>
2019-08-25 01:25:22 -0600

    Better error handling + suggestions API functionality (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Icon, Tooltip, Input} from "antd";
 import logo from './logo.svg';
 import './App.css';
@@ -9,49 +9,57 @@ import entry from './mockedAssets/en-fr/entry';
 
 // TODO - use TypeScript interfaces!
 // TODO - use SCSS and given classnames to change styling
-
 // TODO - styling remove .cit with #{word_id} and children "&nbsp"?
 
 const App: React.FC = () => {
   const content: string = entry.entryContent // temporary content!
   // TODO - change to proper state
-  const [testState, setTestState] = useState('This is the index page!');
-  
+  const [testState, setTestState] = useState('Welcome to QUOI!');
+
   // To be sent to and used by API.
   // TODO - use i18n instead of dictLanguage for button text
   const [dictLanguage, setDictLanguage] = useState('english-french');
 
-
   const sanitizer = DOMPurify.sanitize; // TODO - use more of this
 
   const handleClickTest = () => {
-      // TODO - use i18n instead of dictLanguage for button text
-    console.log('clicked test!');
+    // TODO - use i18n instead of dictLanguage for button text
     // TODO - for toggles, instead of comparing text, maybe we can instead compare ids.
     // ex. ids can be 'en-fr', 'fr-en'
-    if (dictLanguage === "english-french") {
-      setDictLanguage("french-english");
+    if (dictLanguage === 'english-french') {
+      setDictLanguage('french-english');
     }
-    else if (dictLanguage === "french-english") {
-      setDictLanguage("english-french");
+    else if (dictLanguage === 'french-english') {
+      setDictLanguage('english-french');
     }
     else {
-      setDictLanguage("what lol");
+      setDictLanguage('what lol');
     }
   };
 
-  const handleClickSearch = () => {
-    console.log("clicked search!");
-    axios.get('http://127.0.0.1:5000', {
-      headers: {
-        'dict-language': dictLanguage
-      }
-    }).then((response) => {
+  const handleClickSearch = (word: string) => {
+    if (word === null || word.trim() === "") {
+      setTestState('Please enter a word.')
+    } else {
+      getContent(word);
+    }
+  }
+
+  const getContent = (word: string) => {
+    axios.get(`http://127.0.0.1:5000/${dictLanguage}/${word}`)
+    .then((response) => {
       console.log(response);
-      setTestState(response.data); 
-    }).catch(() => {
+        if (!response.data.hasOwnProperty('suggestions')) {
+          setTestState(response.data.entryContent);
+        } else {
+          // TODO - use a new component or tag to display list of suggestions
+          setTestState(`No results available for '${word}' &#128542 \n Did you mean ${response.data.suggestions} ?`);
+        }
+    })
+    .catch(() => {
       // TODO - make error call for actual non-mocked calls
-      setTestState("currently offline!");
+      // TODO - this catch is called if 1. server is offline OR 2. 
+      setTestState(`The server is currently offline. &#128542 Please try again later.`);
     })
   }
 
---
+++ src/mockedAssets/en-fr/entry.js
@@ -0,0 +1,522 @@
+/* eslint-disable no-useless-escape*/
+export default {
+    entryContent: `<div class=\"entry_container\">
+    <div class=\"entry lang_en-gb\" id=\"board_1\">
+    <span class=\"inline\">
+    <h1 class=\"hwd\">board</h1>
+    <span> [</span>
+    <span class=\"pron\" type=\"\">ˈbɔːʳd<a href=\"#\" class=\"playback\">
+    <img src=\"https://api.collinsdictionary.com/external/images/redspeaker.gif?version=2016-11-09-0913\" alt=\"Pronunciation for board\" class=\"sound\" title=\"Pronunciation for board\" style=\"cursor: pointer\"/>
+    </a>
+    <audio type=\"pronunciation\" title=\"board\">
+    <source type=\"audio/mpeg\" src=\"https://api.collinsdictionary.com/media/sounds/sounds/0/055/05581/05581.mp3\"/>Your browser does not support HTML5 audio.</audio>
+    </span>
+    <span>]</span>
+    </span>
+    <div class=\"hom\" id=\"board_1.1\">
+    <span>   </span>
+    <span class=\"gramGrp\">
+    <span class=\"pos\">noun</span>
+    </span>
+    <div class=\"sense\">
+    <span>   </span>
+    <span class=\"bold\">1 </span>
+    <span class=\"lbl\">
+    <span>(</span>wooden<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">planche <em class=\"hi\">f</em>
+    </span>
+    </span>
+    <span class=\"cit\" id=\"board_1.2\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">2 </span>
+    <span>(</span>
+    <em class=\"span\">also</em>
+    <span class=\"inline\">
+    <span class=\"orth\">blackboard</span>
+    </span>
+    <span>)</span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">tableau <em class=\"hi\">m</em> (noir)</span>
+    </span>
+    <span class=\"re\" id=\"board_1.3\">
+    <span>;   </span>
+    <span class=\"inline\">
+    <span class=\"orth\">on the board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">au tableau</span>
+    </span>
+    <span class=\"cit\" id=\"on-the-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">3 </span>
+    <span>(</span>
+    <em class=\"span\">also</em>
+    <span class=\"inline\">
+    <span class=\"orth\">noticeboard</span>
+    </span>
+    <span>)</span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">panneau <em class=\"hi\">m</em> d'affichage</span>
+    </span>
+    <span class=\"cit\" id=\"board_1.5\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">4 </span>
+    <span class=\"lbl\">
+    <span>(</span>for chess<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">échiquier <em class=\"hi\">m</em>
+    </span>
+    </span>
+    <span class=\"cit\" id=\"board_1.6\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">5 </span>
+    <span class=\"re\" id=\"board_1.7\">
+    <span class=\"inline\">
+    <span class=\"orth\">to sweep the board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"lbl\">
+    <span>(</span>in competition, election<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">tout gagner</span>
+    </span>
+    <span class=\"cit\" id=\"to-sweep-the-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">6 </span>
+    <span class=\"lbl\">
+    <span>(= </span>committee<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">conseil <em class=\"hi\">m</em>
+    </span>
+    </span>
+    <span>, </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">comité <em class=\"hi\">m</em>
+    </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">7 </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>of firm<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">conseil <em class=\"hi\">m</em> d'administration</span>
+    </span>
+    <span class=\"cit\" id=\"board_1.9\">
+    <span>;   </span>
+    </span>
+    <span class=\"cit\" id=\"board_1.10\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">8 </span>
+    <span class=\"lbl\">
+    <span>(</span>nautical</span>
+    <span class=\"lbl\">
+    <span>, </span>aviation<span>)</span>
+    </span>
+    <span class=\"re\" id=\"board_1.11\">
+    <span> </span>
+    <span class=\"inline\">
+    <span class=\"orth\">on board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>passenger<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">à bord</span>
+    </span>
+    <span class=\"cit\" id=\"on-board_1.1\">
+    <span>;   </span>
+    </span>
+    <span class=\"cit\" id=\"on-board_1.2\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    <span class=\"re\" id=\"board_1.14\">
+    <span>;   </span>
+    <span class=\"inline\">
+    <span class=\"orth\">to take sth on board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>
+    <span>+ </span>idea, suggestion<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">prendre (bonne) note de qch</span>
+    </span>
+    <span class=\"cit\" id=\"to-take-sth-on-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">9 </span>
+    <span class=\"lbl\">
+    <span>(= </span>meals<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">pension <em class=\"hi\">f</em>
+    </span>
+    </span>
+    <span class=\"re\" id=\"board_1.16\">
+    <span>;   </span>
+    <span class=\"inline\">
+    <span class=\"orth\">with board and lodging</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">logé<span class=\"lbl\">(e)</span> nourri<span>, </span>
+    <span class=\"lbl\">(e)</span>
+    </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    <span class=\"re\" id=\"board_1.17\">
+    <span>;   </span>
+    <span class=\"inline\">
+    <span class=\"orth\">full board</span>
+    <span class=\"lbl\">
+    <span> (</span>British<span>)</span>
+    </span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">pension <em class=\"hi\">f</em> complète</span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    <span class=\"re\" id=\"board_1.18\">
+    <span>;   </span>
+    <span class=\"inline\">
+    <span class=\"orth\">half board</span>
+    <span class=\"lbl\">
+    <span> (</span>British<span>)</span>
+    </span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">demi-pension <em class=\"hi\">f</em>
+    </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">10 </span>
+    <span class=\"re\" id=\"board_1.19\">
+    <span class=\"inline\">
+    <span class=\"orth\">above board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>arrangement, deal<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">régulier<span class=\"lbl\">(‑ière)</span>
+    </span>
+    </span>
+    <span>, </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">dans les règles</span>
+    </span>
+    <span class=\"cit\" id=\"above-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">11 </span>
+    <span class=\"re\" id=\"board_1.21\">
+    <span class=\"inline\">
+    <span class=\"orth\">across the board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"gramGrp\">
+    <span class=\"subc\">adverb</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">systématiquement</span>
+    </span>
+    <span class=\"cit\" id=\"across-the-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span>; </span>
+    <span class=\"gramGrp\">
+    <span class=\"subc\">adjective</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">de portée générale</span>
+    </span>
+    <span class=\"cit\" id=\"across-the-board_1.2\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">12 </span>
+    <span class=\"re\" id=\"board_1.24\">
+    <span class=\"inline\">
+    <span class=\"orth\">to go by the board</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"lbl\">
+    <span>(= </span>be ignored<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">tomber à l'eau</span>
+    </span>
+    <span class=\"cit\" id=\"to-go-by-the-board_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span>; </span>
+    <span class=\"lbl\">
+    <span>(= </span>be unimportant<span>)</span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">compter pour rien</span>
+    </span>
+    <span>, </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">n'avoir aucune importance</span>
+    </span>
+    <span class=\"cit\" id=\"to-go-by-the-board_1.2\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </div>
+    <!-- End of DIV hom-->
+    <div class=\"hom\" id=\"board_1.27\">
+    <span> <br/>▶ </span>
+    <span class=\"gramGrp\">
+    <span class=\"pos\">transitive verb</span>
+    </span>
+    <div class=\"sense\">
+    <span class=\"lbl\">
+    <span>(= </span>go on board<span>)</span>
+    </span>
+    <div class=\"sense\">
+    <span class=\"bold\">   a </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>
+    <span>+ </span>ship, plane<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">monter à bord de</span>
+    </span>
+    <span class=\"cit\" id=\"board_1.28\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span class=\"bold\">   b </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span>[</span>
+    <span>+ </span>train, bus<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">monter dans</span>
+    </span>
+    <span class=\"cit\" id=\"board_1.29\">
+    <span>;   </span>
+    </span>
+    <span class=\"cit\" id=\"board_1.30\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </div>
+    <!-- End of DIV sense-->
+    </div>
+    <!-- End of DIV hom-->
+    <div class=\"hom\" id=\"board_1.31\">
+    <span> <br/>▶ </span>
+    <span class=\"gramGrp\">
+    <span class=\"pos\">intransitive verb</span>
+    </span>
+    <div class=\"sense\">
+    <span>   </span>
+    <span class=\"bold\">1 </span>
+    <span class=\"lbl\">
+    <span>(= </span>go on board<span>)</span>
+    </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span> [</span>air passenger<span>]</span>
+    </span>
+    </span>
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">embarquer</span>
+    </span>
+    <span class=\"cit\" id=\"board_1.32\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    <div class=\"sense\">
+    <span> <br/>
+    </span>
+    <span class=\"bold\">2 </span>
+    <span class=\"lbl\">
+    <span>(= </span>take passengers on board<span>)</span>
+    </span>
+    <span class=\"gramGrp\">
+    <span class=\"colloc\">
+    <span> [</span>flight<span>]</span>
+    </span>
+    </span>
+    <span class=\"re\" id=\"board_1.33\">
+    <span> </span>
+    <span class=\"inline\">
+    <span class=\"orth\">now boarding at gate 13</span>
+    </span>
+    <div class=\"sense\">
+    <span> </span>
+    <span class=\"cit lang_fr\">
+    <span class=\"quote\">embarquement immédiat porte 13</span>
+    </span>
+    <span class=\"cit\" id=\"now-boarding-at-gate-13_1.1\">
+    <span>;   </span>
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </span>
+    </div>
+    <!-- End of DIV sense-->
+    </div>
+    <!-- End of DIV hom-->
+    <span class=\"re\">
+    <span class=\"xr\"> See \n                \n        \t\t<a data-resource=\"english-french\" data-topic=\"board-up_1\" href=\"\">board up</a>
+    </span>
+    </span>
+    </div>
+    <!-- End of DIV entry lang_en-gb-->
+    </div>
+    <!-- End of DIV entry_container-->\n`
+}
