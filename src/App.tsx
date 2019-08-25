2277e269ec7b345499d9fa1d2e9920c6a1eb9c90
kpatenio <25045015+kpatenio@users.noreply.github.com>
2019-08-24 19:24:15 -0600

    Antd components (#1)
    * added mock api html content
    
    * Added some todo comments
    
    * added customer header to dictionary language
    
    * installed antd react components
    
    * added antd searchbar component
    
    * clicking search button calls Flask server

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
+import {Button, Icon, Tooltip, Input} from "antd";
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import DOMPurify from 'dompurify';
+// import parse from 'html-react-parser'; // if using this, reinstall package
+import entry from './mockedAssets/en-fr/entry';
 
-// TODO - convert into a state component!
+// TODO - use TypeScript interfaces!
+// TODO - use SCSS and given classnames to change styling
+
+// TODO - styling remove .cit with #{word_id} and children "&nbsp"?
 
 const App: React.FC = () => {
+  const content: string = entry.entryContent // temporary content!
   // TODO - change to proper state
-  const [testState, setTestState] = useState(null);
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5000/')
-    .then((response) => {
-      console.log(response);
-      setTestState(response.data);
-    });
-  });
+  const [testState, setTestState] = useState('This is the index page!');
+  
+  // To be sent to and used by API.
+  // TODO - use i18n instead of dictLanguage for button text
+  const [dictLanguage, setDictLanguage] = useState('english-french');
 
+
+  const sanitizer = DOMPurify.sanitize; // TODO - use more of this
+
+  const handleClickTest = () => {
+      // TODO - use i18n instead of dictLanguage for button text
+    console.log('clicked test!');
+    // TODO - for toggles, instead of comparing text, maybe we can instead compare ids.
+    // ex. ids can be 'en-fr', 'fr-en'
+    if (dictLanguage === "english-french") {
+      setDictLanguage("french-english");
+    }
+    else if (dictLanguage === "french-english") {
+      setDictLanguage("english-french");
+    }
+    else {
+      setDictLanguage("what lol");
+    }
+  };
+
+  const handleClickSearch = () => {
+    console.log("clicked search!");
+    axios.get('http://127.0.0.1:5000', {
+      headers: {
+        'dict-language': dictLanguage
+      }
+    }).then((response) => {
+      console.log(response);
+      setTestState(response.data); 
+    }).catch(() => {
+      // TODO - make error call for actual non-mocked calls
+      setTestState("currently offline!");
+    })
+  }
+
+  const {Search} = Input;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {testState} </p>
-        {/* //TODO - what are target and rel? */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Tooltip placement="topLeft" title="Click here to swap languages!">
+            <Button onClick={handleClickTest} type="primary">
+              {dictLanguage}
+              <Icon type="swap" />
+            </Button>
+          </Tooltip>
+          <Search
+            className="searchbar"
+            placeholder="Search for a word" 
+            onSearch={handleClickSearch} 
+          />
+
+          <div dangerouslySetInnerHTML={{__html: sanitizer(testState)}}/>
+          {/* //TODO - what are target and rel? */}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
 }
 
 export default App;
