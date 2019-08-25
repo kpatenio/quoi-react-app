600909a76f0c9bacf7f46e5571548f667c35d418
Katherine Patenio <patenio@ualberta.ca>
2019-08-24 18:10:45 -0600

    installed antd react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react';
+import {Button, Icon, Tooltip} from "antd";
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -12,35 +13,40 @@ import entry from './mockedAssets/en-fr/entry';
 // TODO - styling remove .cit with #{word_id} and children "&nbsp"?
 
 const App: React.FC = () => {
-  const content = entry.entryContent // temporary content!
+  const content: string = entry.entryContent // temporary content!
   // TODO - change to proper state
   const [testState, setTestState] = useState('This is the index page!');
-  const [dictLanguage, setDictLanguage] = useState('english-french');
-  useEffect(() => {
-  //     axios.get('https:127.0.0.1:5000/', {
-  //       headers: { // headers to be read by flask app only
-  //         dictToggle: dictToggle
-  //       }
-  //     })
-  //     .then((response) => {
-  //       console.log(response);
-  //       setTestState(response.data.entryContent); // TODO - new branch
-  //     });
-    axios.get('http://127.0.0.1:5000', {
-      headers: {
-        'dict-language': dictLanguage
-      }
-    }).then((response) => {
-      console.log(response);
-      setTestState(response.data); 
-    })
-  });
+
+  const [dictLanguage, setDictLanguage] = useState('English-French');
+  // useEffect(() => {
+  //   axios.get('http://127.0.0.1:5000', {
+  //     headers: {
+  //       'dict-language': dictLanguage
+  //     }
+  //   }).then((response) => {
+  //     console.log(response);
+  //     setTestState(response.data); 
+  //   })
+  // });
   const sanitizer = DOMPurify.sanitize; // TODO - use more of this
 // TODO searchbar - https://dev.to/sage911/how-to-write-a-search-component-with-suggestions-in-react-d20
-  return (
+
+const handleClickTest = () => {
+  setDictLanguage('French-English');
+};
+
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Tooltip placement="topLeft" title="Click here to swap languages!">
+          <Button onClick={handleClickTest} type="primary">
+            {dictLanguage}
+            <Icon type="swap" />
+          </Button>
+        </Tooltip>
+
+
         <div dangerouslySetInnerHTML={{__html: sanitizer(testState)}}/>
         {/* //TODO - what are target and rel? */}
         <a
