857eee0d98588a7d8b0368a89ffdace325d5f71a
Katherine Patenio <patenio@ualberta.ca>
2019-08-24 19:18:16 -0600

    clicking search button calls Flask server

--- src/App.tsx
+++ src/App.tsx
@@ -21,68 +21,70 @@ const App: React.FC = () => {
   // TODO - use i18n instead of dictLanguage for button text
   const [dictLanguage, setDictLanguage] = useState('english-french');
 
-  // useEffect(() => {
-  //   axios.get('http://127.0.0.1:5000', {
-  //     headers: {
-  //       'dict-language': dictLanguage
-  //     }
-  //   }).then((response) => {
-  //     console.log(response);
-  //     setTestState(response.data); 
-  //   })
-  // });
+
   const sanitizer = DOMPurify.sanitize; // TODO - use more of this
-// TODO searchbar - https://dev.to/sage911/how-to-write-a-search-component-with-suggestions-in-react-d20
 
-const handleClickTest = () => {
-    // TODO - use i18n instead of dictLanguage for button text
-  console.log('clicked test!');
-  if (dictLanguage === "english-french") {
-    setDictLanguage("french-english");
+  const handleClickTest = () => {
+      // TODO - use i18n instead of dictLanguage for button text
+    console.log('clicked test!');
+    // TODO - for toggles, instead of comparing text, maybe we can instead compare ids.
+    // ex. ids can be 'en-fr', 'fr-en'
+    if (dictLanguage === "english-french") {
+      setDictLanguage("french-english");
+    }
+    else if (dictLanguage === "french-english") {
+      setDictLanguage("english-french");
+    }
+    else {
+      setDictLanguage("what lol");
+    }
+  };
+
+  const handleClickSearch = () => {
+    console.log("clicked search!");
+    axios.get('http://127.0.0.1:5000', {
+      headers: {
+        'dict-language': dictLanguage
+      }
+    }).then((response) => {
+      console.log(response);
+      setTestState(response.data); 
+    }).catch(() => {
+      // TODO - make error call for actual non-mocked calls
+      setTestState("currently offline!");
+    })
   }
-  else if (dictLanguage === "french-english") {
-    setDictLanguage("english-french");
-  }
-  else {
-    setDictLanguage("what lol");
-  }
-};
 
-const handleClickSearch = () => {
-  console.log("clicked search!")
-}
+  const {Search} = Input;
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Tooltip placement="topLeft" title="Click here to swap languages!">
+            <Button onClick={handleClickTest} type="primary">
+              {dictLanguage}
+              <Icon type="swap" />
+            </Button>
+          </Tooltip>
+          <Search
+            className="searchbar"
+            placeholder="Search for a word" 
+            onSearch={handleClickSearch} 
+          />
 
-const {Search} = Input;
-return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Tooltip placement="topLeft" title="Click here to swap languages!">
-          <Button onClick={handleClickTest} type="primary">
-            {dictLanguage}
-            <Icon type="swap" />
-          </Button>
-        </Tooltip>
-        <Search
-          className="searchbar"
-          placeholder="Search for a word" 
-          onSearch={handleClickSearch} 
-        />
-
-
-        <div dangerouslySetInnerHTML={{__html: sanitizer(testState)}}/>
-        {/* //TODO - what are target and rel? */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div dangerouslySetInnerHTML={{__html: sanitizer(testState)}}/>
+          {/* //TODO - what are target and rel? */}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
 }
 
 export default App;
