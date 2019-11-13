35f42ee9bff514f2416046f7936efec41e50dc0b
kpatenio <patenio@ualberta.ca>
2019-11-13 02:24:58 -0700

    fixed broken tests by using MemoryRouter

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {render} from '@testing-library/react';
+import {MemoryRouter} from 'react-router-dom';
 import App from '../App';
+
+// Use MemoryRouter to maintain react router's context.
+// See here: https://reacttraining.com/react-router/web/guides/testing
 
 describe("just a test", () => {
     it('should pass', () => {
@@ -8,12 +12,20 @@ describe("just a test", () => {
     });
 
     it('should render App', () => {
-        const {container} = render(<App/>);
+        const {container} = render(
+        <MemoryRouter>
+            <App/>
+        </MemoryRouter>
+        );
         expect(container.firstChild).toMatchSnapshot();
     })
 
     it('should render button', () => {
-        const {getByTestId} = render(<App/>);
+        const {getByTestId} = render(
+            <MemoryRouter>
+                <App/>
+            </MemoryRouter>
+            );
         getByTestId('dictionaryToggle');
     })
 })
